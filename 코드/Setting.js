b1cdf0ef5904b8951b7912108706413451211bd7
master <onlybesthome@gmail.com>
2022-11-18 19:14:07 +0900

    ver 0.3.1

--- 코드/Setting.js
+++ 코드/Setting.js
@@ -4,7 +4,7 @@
     const Setting = {
         dataSeperator : "/",
         nodeJS : true,
-        version : "0.3.0",
+        version : "0.3.1",
         itemStack : 20,
         invenLimit : 10
     }
