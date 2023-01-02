616c49f79ad0002077c6eefc29699b2563e1bf3d
master <onlybesthome@gmail.com>
2023-01-02 17:25:22 +0900

    ver 0.6.0 release (제작법 추가)

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ const sqlite3 = require('sqlite3').verbose();
 str = "사과 +123 바나나 +512 키위 +512"
 let a = str.replace(/[가-힣]+\s\+\d+\s/g, v => v.slice(0, -1) + "\n")
 console.log(str)
+let b = "as[]".replace(/\[]/, '')
+console.log(b)
